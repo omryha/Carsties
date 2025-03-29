d2ca0f7c9b86d0de1b23bff77e337ebb2151aabb
omryha <omryha@github.com>
2025-03-29 19:04:52 +0300

    Added client (Next.js)

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,32 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import AuctionCard from './AuctionCard';
-import { Auction, PagedResult } from '../types';
-import AppPagination from '../components/AppPagination';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useShallow } from 'zustand/shallow';
 import { getData } from '../actions/auctioActions';
+import AppPagination from '../components/AppPagination';
+import { useParamsStore } from '../hooks/useParamsStore';
+import { Auction, PagedResult } from '../types';
+import AuctionCard from './AuctionCard';
+import Filters from './Filters';
+import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
-  const [auctions, setAuctions] = useState<Auction[]>([]);
-  const [pageCount, setPageCount] = useState<number>(0);
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [data, setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore(
+    useShallow((state) => ({
+      pageNumber: state.pageNumber,
+      pageSize: state.pageSize,
+      searchTerm: state.searchTerm,
+      orderBy: state.orderBy,
+      filterBy: state.filterBy,
+    }))
+  );
+  const setParams = useParamsStore((state) => state.setParams);
+  const url = qs.stringifyUrl({ url: '', query: params });
+
+  function setPageNumber(pageNumber: number) {
+    setParams({ pageNumber });
+  }
 
   useEffect(() => {
-    getData(pageNumber).then((data) => {
-      setAuctions(data.results);
-      setPageCount(data.pageCount);
+    getData(url).then((data) => {
+      setData(data);
     });
-  }, [pageNumber]);
+  }, [url]);
+
+  if (!data) return <h3>Loading...</h3>;
 
   return (
     <>
-      <div className='grid grid-cols-4 gap-6'>
-        {data &&
-          data.results.map((auction) => (
-            <AuctionCard auction={auction} key={auction.id} />
-          ))}
-      </div>
-      <div className='flex justify-center mt-4'>
-        <AppPagination currentPage={1} pageCount={data.pageCount} />
-      </div>
+      <Filters />
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset></EmptyFilter>
+      ) : (
+        <>
+          <div className='grid grid-cols-4 gap-6'>
+            {data.results.map((auction) => (
+              <AuctionCard auction={auction} key={auction.id} />
+            ))}
+          </div>
+          <div className='flex justify-center mt-4'>
+            <AppPagination
+              pageChanged={setPageNumber}
+              currentPage={params.pageNumber}
+              pageCount={data.pageCount}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
---
+++ frontend/web-app/app/components/EmptyFilter.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React from 'react';
+import Heading from './Heading';
+import { Button } from 'flowbite-react';
+import { useParamsStore } from '../hooks/useParamsStore';
+
+type Props = {
+  title?: string;
+  subtitle?: string;
+  showReset?: boolean;
+  showLogin?: boolean;
+  callbackUrl?: string;
+};
+
+export default function EmptyFilter({
+  title = 'No matches for this filter',
+  subtitle = 'Try changing or resetting the filter',
+  showReset,
+  showLogin,
+  callbackUrl,
+}: Props) {
+  const reset = useParamsStore((state) => state.reset);
+
+  return (
+    <div className='h-[40vh] flex flex-col gap-2 justify-center items-center shadow-lg'>
+      <Heading title={title} subtitle={subtitle} center />
+      <div className='mt-4'>
+        {showReset && (
+          <Button outline onClick={reset}>
+            Remove Filters
+          </Button>
+        )}
+        {showLogin && (
+          <Button outline onClick={reset}>
+            Login
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
